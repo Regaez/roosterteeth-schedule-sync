eff68be95095919280aded259425e93b792825b2
Thomas Threadgold <tj.threadgold@gmail.com>
2020-02-22 21:02:39 +0100

    remove chrome deeplink from event description

--- src/helpers.js
+++ src/helpers.js
@@ -39,7 +39,7 @@ const formatDescription = ({ show_title, display_title, channel_slug, descriptio
   const duration = moment.duration(length, 'seconds').humanize();
   const videoUrl = `${process.env.RT_SITE_URL}${slug}`;
 
-  return `${getChannelName(channel_slug)}${show_title ? ": " + show_title : ''}\n${display_title}\nDuration: ${duration}\n\n${description}\n\nWatch video: googlechrome://navigate?url=${videoUrl}`;
+  return `${getChannelName(channel_slug)}${show_title ? ": " + show_title : ''}\n${display_title}\nDuration: ${duration}\n\n${description}\n\nWatch video: ${videoUrl}`;
 }
 
 const formatLivestreamDescription = ({ title, channel_slug }, slug) => {
